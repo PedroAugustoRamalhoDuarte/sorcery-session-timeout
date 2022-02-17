70b124d95f7bcc11d24b3920472cc2834a25736c
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2022-02-17 18:19:43 -0300

    UserSessions Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :users
-  root :to => 'users#index'
+  root to: 'users#index'
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'login' => "user_sessions#create"
+  post 'logout' => 'user_sessions#destroy', :as => :logout
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
